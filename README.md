136edf95dda34176116be2d2fbffe8490a07ebc7
Adela Tatum <80791686+addy-luna@users.noreply.github.com>
2024-05-11 06:46:16 -0700

    Updates to deleteTask and cleaned up readme

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Task Manager CLI tool
+# To-Do CLI Tool with Node.js and MongoDB
+
+Welcome to the To-Do CLI Tool repository! This project is a command-line interface (CLI) tool built using Node.js, designed to manage your to-do tasks efficiently. It utilizes MongoDB as the database to store all your to-dos securely. Additionally, we make use of several helpful npm packages to enhance the functionality and user experience of the tool.
+
+## Dependencies
+
+We leverage the following npm packages to power various aspects of the To-Do CLI Tool:
+
+-   **commander:**  
+    This package helps us build the CLI tool by providing a framework for defining commands and options.
+    
+-   **chalk:**  
+    Chalk makes messages in the terminal colorful and easy to read, enhancing the overall user experience.
+    
+-   **inquirer:**  
+    Inquirer simplifies user input by providing a powerful interface for prompting users and handling their responses.
+    
+-   **ora:**  
+    Ora adds a touch of elegance to the terminal interface by displaying nice spinning animations, providing visual feedback to users during long-running tasks.
--- commands/deleteTask.js
+++ commands/deleteTask.js
@@ -5,3 +5,57 @@ import {connectDB, disconnectDB} from '../db/connectDB.js'
 import ora from "ora";
 import chalk from "chalk";
 
+/* getTaskCode() is an asynchronous function. The role of this function is to prompt the user to enter the code of the todo that needs
+to be deleted using inquirer.
+The function uses trim() method and returns the trimmed code.
+*/
+
+export async function getTaskCode(){
+    try {
+        // Prompting the user to enter the todo code
+        const answers = await inquirer.prompt([
+            {name: 'code', 'message': 'Enter the code of the todo: ', type: 'input'},
+        ])
+
+        // Trimming user's response so that the todo code does not contain any starting or trailing white spaces
+        answers.code = answers.code.trim()
+
+        return answers
+    } catch (error) {
+        console.log('Something went wrong...\n', error)
+    }
+}
+
+export default async function deleteTask(){
+    try {
+        // Obtaining the todo code provided by user
+        const userCode = await getTaskCode()
+
+        // Connecting to the database
+        await connectDB()
+
+        // Starting the spinner
+        const spinner = ora('Finding and Deleting the todo...').start()
+
+        // Deleting the task
+        const response = await Todos.deleteOne({code: userCode.code})
+
+        // Stopping the spinner
+        spinner.stop()
+
+        // Checking the delete operation
+        if(response.deletedCount === 0){
+            console.log(chalk.redBright('Could not find any todo matching the provided name. Deletion failed.'))
+        } else {
+            console.log(chalk.greenBright('Deleted Task Successfully'))
+        }
+
+        // Disconnecting from the database
+        await disconnectDB()
+    } catch (error) {
+        // Error Handling
+        console.log('Something went wrong, Error: ', error)
+        process.exit(1)
+    }
+}
+
